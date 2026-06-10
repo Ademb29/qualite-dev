aae1cdec5b75e2b66240100e16822b1936497d66
Meyruem <mohammedbeqazr06@gmail.com>
2026-06-10 14:13:00 +0200

    TP2 : tests JUnit pour StatistiqueImpl
    Tests de prixMoyen() : une voiture, plusieurs voitures, et cas d'erreur
    sans voiture (ArithmeticException).

--- testsLogiciel/src/test/java/com/example/demo/service/StatistiqueTests.java
+++ testsLogiciel/src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -1,17 +1,40 @@
 package com.example.demo.service;
 
 import com.example.demo.data.Voiture;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-
-@SpringBootTest
 public class StatistiqueTests {
 
-    @MockBean
-    StatistiqueImpl statistiqueImpl;
+    private StatistiqueImpl statistique;
 
+    @BeforeEach
+    void setUp() {
+        statistique = new StatistiqueImpl();
+    }
+
+    @Test
+    void prixMoyenUneVoiture() throws ArithmeticException {
+        statistique.ajouter(new Voiture("BMW", 30000));
+        Echantillon e = statistique.prixMoyen();
+        assertEquals(1, e.getNombreDeVoitures());
+        assertEquals(30000, e.getPrixMoyen());
+    }
+
+    @Test
+    void prixMoyenPlusieursVoitures() throws ArithmeticException {
+        statistique.ajouter(new Voiture("BMW", 10000));
+        statistique.ajouter(new Voiture("Audi", 20000));
+        statistique.ajouter(new Voiture("Peugeot", 30000));
+        Echantillon e = statistique.prixMoyen();
+        assertEquals(3, e.getNombreDeVoitures());
+        assertEquals(20000, e.getPrixMoyen());
+    }
+
+    @Test
+    void prixMoyenSansVoitureLanceException() {
+        assertThrows(ArithmeticException.class, () -> statistique.prixMoyen());
+    }
 }
